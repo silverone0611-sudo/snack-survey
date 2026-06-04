2de14b9b5642e92806299f37b7445a195586dc99
silve <silve@example.com>
2026-06-04 20:05:50 +0900

    update final gift notice

--- src/App.jsx
+++ src/App.jsx
@@ -3506,17 +3506,10 @@ function EventEntryStep({
       {hasVoiceQuizAnswer ? (
       <div className="event-final-box">
       <strong>설문 참여자 전원 선물</strong>
-       <p>
-       소정의{" "}
-      <strong style={{ color: "#d32f2f" }}>선물</strong>
-       이 준비되어 있습니다.<br />
-       {" "}
-      <strong style={{ color: "#d32f2f" }}>설문을 완료</strong>
-       한 학생은{" "}
-      <strong style={{ color: "#d32f2f" }}>1층 교무실</strong>
-        {" 김선미 선생님께 찾아가 "}
-      <strong style={{ color: "#d32f2f" }}>선물</strong>
-       을 받아가세요.
+       <p className="event-entry-note">
+  소정의 <strong style={{ color: "#d32f2f" }}>선물</strong>이 준비되어 있습니다.<br />
+  <strong style={{ color: "#d32f2f" }}>설문을 완료</strong>한 학생은 <strong style={{ color: "#d32f2f" }}>1층 교무실</strong>{" "}
+  김선미 선생님께 찾아가 <strong style={{ color: "#d32f2f" }}>선물</strong>을 받아가세요.
       </p>
 
         <strong>특별 이벤트 응모</strong>
